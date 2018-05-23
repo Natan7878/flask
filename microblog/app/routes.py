9de49182c166a406fcbc0d45da3d65a187159fbe
Natan <natan78787878@mail.ru>
2018-05-23 15:25:21 -0400

    Flask if statement

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -6,4 +6,4 @@ from app import app
 @app.route('/index')
 def index():
     user = {"username": "Natan"}
-    return render_template('index.html', title='Home', user=user)
+    return render_template('index.html',  title='Home', user=user)
